547cf3c59beb2f9318889a7877f7fdacf131a9b8
unknown <guptasurajm07@gmaile.com>
2022-11-09 18:35:44 +0530

    my portfolio

---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import { Button } from '@chakra-ui/button';
+import { useColorMode } from '@chakra-ui/color-mode'
+import { Image } from '@chakra-ui/image';
+import { Stack, Circle, Flex, Box, Text } from '@chakra-ui/layout';
+import { useMediaQuery } from '@chakra-ui/media-query';
+import React from 'react'
+
+function Header() {
+
+    const { colorMode } = useColorMode();
+    const isDark = colorMode === "dark";
+
+    const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
+
+    return (
+        <Stack>
+            <Circle position="absolute" bg="blue.100" opacity="0.1"
+                w="300px" h="300px" alignSelf="flex-end" />
+            <Flex direction={isNotSmallerScreen ? "row" : "column"}
+                spacing="200px" p={isNotSmallerScreen ? "32" : "0"}
+                alignSelf="flex-start">
+                <Box mt={isNotSmallerScreen ? "0" : 16} align='flex-start'>
+                    <Text fontSize="5xl" fontWeight="semibold">Hi 👋 , I am</Text>
+                    <Text fontSize="7xl" fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, purple.600)" bgClip='text' >Suraj Gupta</Text>
+                    <Text color={isDark ? "gray.200" : "gray.500"}> FULL STACK DEVELOPER </Text>
+                    <Button mt={8} colorScheme="blue" onClick={() =>
+                        window.open("https://drive.google.com/file/d/1fEQ7krFok9YEqLUBgDfOUSqYtSjMPlqI/view?usp=share_link")
+                    }>My Resume</Button>
+
+                </Box>
+                <Image alignSelf="center" mt={isNotSmallerScreen ? "0" : "12"}
+                    mb={isNotSmallerScreen ? "0" : "12"} borderRadius='full'
+                    backgroundColor="transparent" boxShadow="lg"
+                    boxSize="300px" src='https://avatars.githubusercontent.com/u/82599987?s=400&u=96eb724a6b4c01d624e1099ff807aaad6002ac28&v=4' />
+            </Flex>
+
+        </Stack>
+    )
+}
+
+export default Header
